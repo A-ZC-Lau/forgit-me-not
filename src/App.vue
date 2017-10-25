b972ec5f6e467ecab514128da0da0651adf83ccb
alex <alexanderlau25@hotmail.com>
2017-10-25 18:36:42 +1100

    Attempting to use fs

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,171 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
-        <router-view/>
+        <aside id="sidebar">
+            Side bar
+        </aside>
+        <router-view class='main-view'/>
     </div>
 </template>
 
 <script>
     const electron = window.require('electron')
+    // const fs = window.require("fs")
+    const { Menu, MenuItem } = electron.remote
+    import $ from 'jquery'
+    require('jquery-ui/ui/widgets/resizable.js')
+    require('jquery-ui/themes/base/resizable.css')
+    // import('jquery-ui/')
 
     export default {
         name: 'app',
         created() {
-            const mainMenuTemplate = []
-            mainMenuTemplate.push({
-              label: 'Developer Tools',
-              submenu:[
+            const template = [
                 {
-                  role: 'reload'
+                    label: "File",
+                    submenu: [
+                        {role: 'save'}
+                    ]
                 },
                 {
-                  label: 'Toggle DevTools',
-                  accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-                  click(item, focusedWindow){
-                    focusedWindow.toggleDevTools();
-                  }
+                    label: 'Edit',
+                    submenu: [
+                        {role: 'undo'},
+                        {role: 'redo'},
+                        {type: 'separator'},
+                        {role: 'cut'},
+                        {role: 'copy'},
+                        {role: 'paste'},
+                        {role: 'pasteandmatchstyle'},
+                        {role: 'delete'},
+                        {role: 'selectall'}
+                    ]
+                },
+                {
+                    label: 'View',
+                    submenu: [
+                        {type: 'separator'},
+                        {role: 'resetzoom'},
+                        {role: 'zoomin'},
+                        {role: 'zoomout'},
+                        {type: 'separator'},
+                        {role: 'togglefullscreen'}
+                    ]
+                },
+                {
+                    role: 'window',
+                    submenu: [
+                        {role: 'minimize'},
+                        {role: 'close'}
+                    ]
+                },
+                {
+                    role: 'help',
+                    submenu: [
+                        {
+                            label: 'Learn More',
+                            click () { electron.shell.openExternal('https://electron.atom.io') }
+                        }
+                    ]
+                },
+                {
+                    label: 'Developer Tools',
+                    submenu:[
+                        {role: 'reload'},
+                        {role: 'forcereload'},
+                        {role: 'toggledevtools'}
+                    ]
+                },
+            ]
+
+            if (electron.remote.process.platform === 'darwin') {
+              template.unshift({
+                label: app.getName(),
+                submenu: [
+                  {role: 'about'},
+                  {type: 'separator'},
+                  {role: 'services', submenu: []},
+                  {type: 'separator'},
+                  {role: 'hide'},
+                  {role: 'hideothers'},
+                  {role: 'unhide'},
+                  {type: 'separator'},
+                  {role: 'quit'}
+                ]
+              })
+
+              // Edit menu
+              template[1].submenu.push(
+                {type: 'separator'},
+                {
+                  label: 'Speech',
+                  submenu: [
+                    {role: 'startspeaking'},
+                    {role: 'stopspeaking'}
+                  ]
                 }
+              )
+
+              // Window menu
+              template[3].submenu = [
+                {role: 'close'},
+                {role: 'minimize'},
+                {role: 'zoom'},
+                {type: 'separator'},
+                {role: 'front'}
               ]
+            }
+
+            const menu = Menu.buildFromTemplate(template)
+            Menu.setApplicationMenu(menu)
+
+
+            // pop up menu
+            const popup_menu = new Menu()
+            popup_menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
+            popup_menu.append(new MenuItem({type: 'separator'}))
+            popup_menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+
+            window.addEventListener('contextmenu', (e) => {
+              e.preventDefault()
+              popup_menu.popup(electron.remote.getCurrentWindow())
+            }, false)
+        },
+        mounted () {
+            $( "#sidebar" ).resizable({
+                maxHeight: "100vh",
+                minWidth: 100
             });
-            const mainMenu = electron.remote.Menu.buildFromTemplate(mainMenuTemplate);
-            // Insert menu
-            electron.remote.Menu.setApplicationMenu(mainMenu);
         }
     }
 </script>
 
 <style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+    body, html {
+        margin: 0px;
+        padding: 0px;
+    }
+</style>
+<style scoped>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        display: flex;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    aside {
+        background-color: lightblue;
+        resize: horizontal;
+        width: 250px;
+    }
+
+    .main-view {
+        background-color: orange;
+        flex-grow: 1;
+    }
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template src="./templates/Main.html">
+</template>
+
+<script>
+    const electron = window.require('electron')
+    const fs = window.require('fs')
+
+    export default {
+        methods: {
+            open_folder() {
+                // alert('open folder')
+                let folder = electron.remote.dialog.showOpenDialog({properties: ['openDirectory']});
+                console.log(folder[0]);
+                // fs.readdir('.', function(dir) {
+                //     console.log(dir);
+                //     // es6
+                //     //   for(let filePath of dir) {
+                //     //     console.log(filePath);
+                //     //   }
+                // });
+                fs.writeFile('./fs-test.txt', 'fs test', (err) => console.log('could not write', err))
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
