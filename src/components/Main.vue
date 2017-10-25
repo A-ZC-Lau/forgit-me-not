c728552f1582835ac9eedbd47e19a18197307237
alex <alexanderlau25@hotmail.com>
2017-10-26 00:14:59 +1100

    [src/components/Main.vue] getting ready to accept file from store

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,22 +4,15 @@
 <script>
     const electron = window.require('electron')
     const fs = window.require('fs')
+    import store from '@/store'
 
     export default {
-        methods: {
-            open_folder() {
-                // alert('open folder')
-                let folder = electron.remote.dialog.showOpenDialog({properties: ['openDirectory']});
-                console.log(folder[0]);
-                // fs.readdir('.', function(dir) {
-                //     console.log(dir);
-                //     // es6
-                //     //   for(let filePath of dir) {
-                //     //     console.log(filePath);
-                //     //   }
-                // });
-                fs.writeFile('./fs-test.txt', 'fs test', (err) => console.log('could not write', err))
+        data() {
+            return {
+                store
             }
+        },
+        methods: {
         }
     }
 </script>
