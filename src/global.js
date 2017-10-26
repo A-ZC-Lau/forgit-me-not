1f59dce54501aee9d982ea401c55b7b909b2573d
lau-a <alexander.lau@taguchimail.com>
2017-10-27 10:15:02 +1100

    [src/global.js] added save_file

--- src/global.js
+++ src/global.js
@@ -3,6 +3,23 @@ const path = require('path')
 const fs = window.require('fs')
 import store from '@/store'
 
+export var save_file = function ()
+{
+    console.log('saving')
+    let { root, folder, file } = store.state.General
+    if (folder === "chapters")
+    {
+        let location = path.join(root, folder, file);
+        let content = $("#textarea").val()
+        fs.writeFile(location, content, (err) => {
+            if (err)
+            {
+                console.error(err)
+            }
+        })
+    }
+}
+
 export var select_root = function() {
     let the_root = electron.remote.dialog.showOpenDialog({properties: ['openDirectory']});
     if (!the_root)
