948ca06f74ee81f2b5a5a81fe472d8a290b38d79
lau-a <alexander.lau@taguchimail.com>
2017-10-27 15:35:31 +1100

    [src/components/Input.vue] new file, for new BrowserWindow

---
+++ src/components/Input.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>
+            Name:
+        </div>
+        <input type="text" />
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
