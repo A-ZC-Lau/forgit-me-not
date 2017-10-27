30e9af7482d5065d0821752a60ba60c48c313ce3
lau-a <alexander.lau@taguchimail.com>
2017-10-27 11:08:31 +1100

    [src/components/Modal.vue] moved modal to its own file

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- This is the modal -->
+    <div id="my-id" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+            <h2 class="uk-modal-title">Add Group</h2>
+            <div class="uk-modal-body">
+                <div>
+                    Name:
+                </div>
+                <input type="text" />
+            </div>
+            <div class="uk-modal-footer">
+                <div class="uk-float-right">
+                    <button class="uk-modal-close" type="button">Ok</button>
+                    <button class="uk-modal-close" type="button">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+</style>
