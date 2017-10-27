5a7dcdb1cdff55764ecfaa77d3910451db215d9d
lau-a <alexander.lau@taguchimail.com>
2017-10-27 15:35:51 +1100

    [src/router/index.js] added input

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
 import Main from '@/components/Main'
+import Input from '@/components/Input'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Main',
-      component: Main
-    }
+      {
+          path: '/main',
+          name: 'Main',
+          component: Main
+      },
+      {
+          path: '/input',
+          name: 'Input',
+          component: Input
+      }
   ]
 })
