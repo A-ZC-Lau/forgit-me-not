41668f858b2460895e55e6b3a83dff6be98016f9
alex <alexanderlau25@hotmail.com>
2017-10-29 01:20:58 +1100

    [src/components/Modal.vue] updated modal contents

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,17 +1,17 @@
 <template>
     <!-- This is the modal -->
-    <div id="my-id" uk-modal class="uk-modal">
+    <div id="my-id" uk-modal class="uk-modal" style="margin: 0;">
         <div class="uk-modal-dialog uk-modal-body">
-            <h2 class="uk-modal-title">Add Group</h2>
+            <h2 class="uk-modal-title">Add {{ type }}</h2>
             <div class="uk-modal-body">
                 <div>
                     Name:
                 </div>
-                <input type="text" />
+                <input type="text" ref="input" />
             </div>
             <div class="uk-modal-footer">
                 <div class="uk-float-right">
-                    <button class="uk-modal-close" type="button">Ok</button>
+                    <button class="uk-modal-close" @click="return_item" type="button">Ok</button>
                     <button class="uk-modal-close" type="button">Cancel</button>
                 </div>
             </div>
@@ -21,7 +21,15 @@
 
 <script>
     export default {
-
+        props: [
+            "add_item",
+            "type"
+        ],
+        methods: {
+            return_item() {
+                this.add_item(this.$refs.input.value)
+            }
+        }
     }
 </script>
 
