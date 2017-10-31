5422c8f62a27e3a54742a275f788a6bb29ed6728
lau-a <alexander.lau@taguchimail.com>
2017-10-31 17:21:25 +1100

    [src/components/Main/Writing/CollectionList/] new folder/files

---
+++ src/components/Main/Writing/CollectionList/CollectionList.vue
@@ -0,0 +1,40 @@
+<template src='./CollectionList.html'></template>
+
+<script>
+    import store from '@/store'
+
+    export default {
+        name: "CollectionList",
+        data() {
+            return {
+                store,
+                type: null
+            }
+        },
+        methods: {
+            change_type(type) {
+                this.type = type
+            }
+        },
+        mounted() {
+            console.log(store.state.General.folder)
+        }
+    }
+</script>
+
+<style>
+    fieldset:before {
+        content: attr(title);
+    }
+
+    fieldset {
+        border: none;
+        font-weight: bold;
+        margin: 0;
+    }
+
+    fieldset > article {
+        font-weight: normal;
+        margin-left: 1em;
+    }
+</style>
